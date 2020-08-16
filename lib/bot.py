7e0fded5c2fb12859b48e79ab51a852359e935ab
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2020-08-16 08:52:32 +0000

    version incremented

--- lib/bot.py
+++ lib/bot.py
@@ -270,4 +270,8 @@ class Bot:
                     bid, _ = order_book["bids"][0]
                 except (IndexError, ValueError) as error:
                     # if the order_book is empty we'll get an exception here
-                    logging.debug(f"
+                    logging.debug(f"{coin.symbol} {error}")
+                    return False
+                logging.debug(f"bid: {bid}")
+                logging.info(
+                    f"{now}: {coin.symbol} [SELLING] {coin.vol
