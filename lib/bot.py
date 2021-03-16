e0c5fff51c4ee4fb84180204bdd34e2508c8fc66
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2021-03-16 08:27:34 +0000

    [Dev Deps] update `jscs`, `eslint`, `@ljharb/eslint-config`

--- lib/bot.py
+++ lib/bot.py
@@ -1043,4 +1043,10 @@ class Bot:
             # and also reduce the TRAIL_TARGET_SELL_PERCENTAGE in the same
             # way we reduced our SELL_AT_PERCENTAGE.
             # We're fine with this one going close to 0.
-            coin.trail_target_sell_percen
+            coin.trail_target_sell_percentage = (
+                add_100(
+                    percent(
+                        ttl,
+                        float(
+                            self.tickers[coin.symbol][
+                         
