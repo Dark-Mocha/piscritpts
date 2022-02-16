b15c2160bb32f1ca2ed160d021f41b6ad8ae970a
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-02-16 19:10:40 +0000

    fixed cs issues

--- lib/coin.py
+++ lib/coin.py
@@ -7,4 +7,13 @@ from lib.helpers import add_100
 class Coin:  # pylint: disable=too-few-public-methods
     """Coin Class"""
 
-    offset: Optional[Dict
+    offset: Optional[Dict[str, int]] = {"s": 60, "m": 3600, "h": 86400}
+
+    def __init__(
+        self,
+        symbol: str,
+        date: float,
+        market_price: float,
+        buy_at: float,
+        sell_at: float,
+     
