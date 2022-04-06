06735f2838f3e699794257c5ff30c4254cb8e181
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-04-06 13:32:50 +0000

    fixed bug in test

--- lib/helpers.py
+++ lib/helpers.py
@@ -8,4 +8,11 @@ from functools import lru_cache
 from os.path import exists, getctime
 from time import sleep, time
 
-imp
+import udatetime
+from binance.client import Client
+from filelock import SoftFileLock
+from tenacity import retry, wait_exponential
+
+
+def mean(values: list[float]) -> float:
+    """returns the mean value of an array of in
