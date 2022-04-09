6f368c80a63d6b2a9c1017ad963ee878b2bb5996
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-04-09 18:43:38 +0000

    Added dependencies

--- lib/helpers.py
+++ lib/helpers.py
@@ -15,4 +15,11 @@ from tenacity import retry, wait_exponential
 
 
 def mean(values: list[float]) -> float:
-    """returns the mean value of an array of in
+    """returns the mean value of an array of integers"""
+    return sum(values) / len(values)
+
+
+@lru_cache(1024)
+def percent(part: float, whole: float) -> float:
+    """returns the percentage value of a number"""
+    result: float = float(whole) / 100 
