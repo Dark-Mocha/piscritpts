39b2aaae60f02c5c89a56a4bba7cdef1852e8ff1
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-09-03 21:30:03 +0000

    merge from 1.2

--- strategies/BuyDropSellRecoveryStrategyWhenBTCisUp.py
+++ strategies/BuyDropSellRecoveryStrategyWhenBTCisUp.py
@@ -2,3 +2,12 @@
 from lib.bot import Bot
 from lib.coin import Coin
 from lib.helpers import c_from_timestamp, logging, percent
+
+
+class Strategy(Bot):
+    """BuyDropSellRecoveryStrategyWhenBTCisUp"""
+
+    def buy_strategy(self, coin: Coin) -> bool:
+        """BuyDropSellRecoveryStrategyWhenBTCisUp buy_strategy
+
+        this strategy 
