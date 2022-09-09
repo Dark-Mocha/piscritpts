50f2f69666f0e1a6be0ecef88488813c15c1ebbc
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-09-09 13:59:27 +0000

    Create deleteme.txt

--- strategies/BuyDropSellRecoveryStrategyWhenBTCisUp.py
+++ strategies/BuyDropSellRecoveryStrategyWhenBTCisUp.py
@@ -13,4 +13,8 @@ class Strategy(Bot):
         this strategy only buys coins when the price of bitcoin is heading up.
         it waits until BTC has gone up by KLINES_SLICE_PERCENTAGE_CHANGE in
         the KLINES_TREND_PERIOD before looking at coin prices.
-    
+        Then as the price of a coin has gone down by the BUY_AT_PERCENTAGE
+        it marks the coin as TARGET_DIP.
+        wait for the coin to go up in price by TRAIL_RECOVERY_PERCENTAGE
+        before buying the coin
+
