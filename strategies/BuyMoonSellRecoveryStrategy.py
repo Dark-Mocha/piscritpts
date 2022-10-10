5547fbdccea97ad5b76c4207a36a2d025d2ea5bf
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-10-10 07:07:36 +0000

    adding todo

--- strategies/BuyMoonSellRecoveryStrategy.py
+++ strategies/BuyMoonSellRecoveryStrategy.py
@@ -1,1 +1,11 @@
-""" bot buy strategy f
+""" bot buy strategy file """
+from lib.bot import Bot
+from lib.coin import Coin
+from lib.helpers import percent
+
+
+class Strategy(Bot):
+    """BuyMoonSellRecoveryStrategy"""
+
+    def buy_strategy(self, coin: Coin) -> bool:
+        """BuyMoonSellRecove
