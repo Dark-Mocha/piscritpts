536ef4b19937c7eebd638db0b85e2892491d6cdf
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-10-10 18:33:27 +0000

    ...cosmetic changes...

--- strategies/BuyOnRecoveryAfterDropDuringGrowthTrendStrategy.py
+++ strategies/BuyOnRecoveryAfterDropDuringGrowthTrendStrategy.py
@@ -10,4 +10,8 @@ class Strategy(Bot):
     def buy_strategy(self, coin: Coin) -> bool:
         """BuyOnRecoveryAfterDropDuringGrowthTrendStrategy buy_strategy
 
-        This strategy looks 
+        This strategy looks for coins that have gone up by
+        KLINES_SLICE_PERCENTAGE_CHANGE in each slice (m,h,d) of the
+        KLINES_TREND_PERIOD.
+        Then it checkous that the current price for those is
+        lower than the BUY_AT
