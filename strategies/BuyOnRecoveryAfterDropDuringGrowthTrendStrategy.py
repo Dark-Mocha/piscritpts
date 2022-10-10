9828b13bbc44ce8aa3fba47d5fae8f9162b0ca3d
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2022-10-10 20:39:56 +0000

    Add missing includes

--- strategies/BuyOnRecoveryAfterDropDuringGrowthTrendStrategy.py
+++ strategies/BuyOnRecoveryAfterDropDuringGrowthTrendStrategy.py
@@ -14,4 +14,9 @@ class Strategy(Bot):
         KLINES_SLICE_PERCENTAGE_CHANGE in each slice (m,h,d) of the
         KLINES_TREND_PERIOD.
         Then it checkous that the current price for those is
-        lower than the BUY_AT
+        lower than the BUY_AT_PERCENTAGE over the maximum price recorded.
+        if it is, then mark the coin as TARGET_DIP
+        and buy it as soon we're over the dip by TRAIL_RECOVERY_PERCENTAGE.
+        """
+
+        u
