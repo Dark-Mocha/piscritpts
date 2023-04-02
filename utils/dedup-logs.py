b1b9deaecd6e01ab31bed397c53c58de64b322ce
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2023-04-02 10:11:05 +0000

    compilable

--- utils/dedup-logs.py
+++ utils/dedup-logs.py
@@ -7,4 +7,13 @@ import traceback
 from pathlib import Path
 
 if __name__ == "__main__":
-  
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-l", "--log", help="log")
+        args = parser.parse_args()
+
+        p = Path(".")
+
+        coin = {}
+        oldcoin = {}
+        w
