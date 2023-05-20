9e97cac32f1d140f3584a354ffbd6ec16ea955b8
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2023-05-20 12:35:31 +0000

    Improved responsiveness

--- utils/split_klines_into_symbol_logs.py
+++ utils/split_klines_into_symbol_logs.py
@@ -20,4 +20,11 @@ coin_filenames = set()
 for line in lines:
     parts = line.split(" ")
     symbol = parts[2]
-    price = parts[
+    price = parts[3]
+
+    date = parts[0].replace("-", "")
+    coin_filename = f"log/{symbol}/{date}.log"
+
+    # if we already have a gzipped file for this coin, it means we've already
+    # processed it, so skip it
+    if os
