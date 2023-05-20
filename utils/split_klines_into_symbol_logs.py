88d28b4f83bf4fa600498d9f1db0b31e19490b87
Dark-Mocha <93438136+Dark-Mocha@users.noreply.github.com>
2023-05-20 09:21:13 +0000

    API improvements

--- utils/split_klines_into_symbol_logs.py
+++ utils/split_klines_into_symbol_logs.py
@@ -9,4 +9,15 @@ from typing import Dict
 parser = argparse.ArgumentParser()
 parser.add_argument("-f")
 
-args = parser.parse_
+args = parser.parse_args()
+daylog = args.f
+
+with gzip.open(daylog, "rt") as f:
+    lines = f.readlines()
+
+coins: Dict = {}
+coin_filenames = set()
+for line in lines:
+    parts = line.split(" ")
+    symbol = parts[2]
+    price = parts[
